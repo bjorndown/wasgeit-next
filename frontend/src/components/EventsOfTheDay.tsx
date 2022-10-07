bb43be74d629cd72e4d2528a8267ac478ff51160
Björn Mosler <dev@bjoern.mosler.ch>
2022-10-07 14:09:06 +0200

    frontend: show link to quickly add event to calendar.

--- frontend/src/components/EventsOfTheDay.tsx
+++ frontend/src/components/EventsOfTheDay.tsx
@@ -1,6 +1,7 @@
-import { format, isSameYear, parseISO } from 'date-fns'
-import { de } from 'date-fns/locale'
 import { Event, ISODate } from '@wasgeit/common/src/types'
+import { format, formatISO, isSameYear, parseISO } from 'date-fns'
+import { de } from 'date-fns/locale'
+import { generateIcalEntry } from '../lib/ical'
 
 type Props = {
   date: ISODate
@@ -20,15 +21,34 @@ export const EventsOfTheDay = ({ date, events }: Props) => {
     <article id={`date-${date}`} className="events-of-the-day" data-day={date}>
       <h2>{formatDateLong(parseISO(date))}</h2>
       {events.map((event) => (
-        <a key={event.url} href={event.url}>
-          <article className="event" data-start-date={event.start}>
+        <article
+          className="event"
+          data-start-date={event.start}
+          key={event.url}
+        >
+          <a key={event.url} href={event.url}>
             <h3 className="event-title">{event.title}</h3>
-            <span className="venue-name">{event.venue}</span>
-          </article>
-        </a>
+          </a>
+          <a href={new URL(event.url).origin} className="venue-name">
+            {event.venue}
+          </a>
+          <a
+            href={generateIcalEntry(event)}
+            download={`${event.start}-${event.venue}.ics`}
+            type="text/calendar"
+            className="calendar-entry"
+          >
+            grad ischribe!
+          </a>
+        </article>
       ))}
 
       <style jsx>{`
+        .calendar-entry {
+          font-size: 1.1rem;
+          align-self: flex-end;
+        }
+
         h2 {
           font-size: var(--medium-font-size);
           text-transform: uppercase;
---
+++ frontend/src/lib/ical.ts
@@ -0,0 +1,28 @@
+import { Event } from '@wasgeit/common/src/types'
+import { endOfDay, parseISO } from 'date-fns'
+
+const toIcalDate = (isoString: string): string =>
+  isoString
+    .replaceAll(/[-:]/g, '')
+    .replaceAll('.000', '')
+    .replaceAll('.999', '')
+
+export const generateIcalEntry = (event: Event): string => {
+  const ical = `BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//wasgeit.ch//NONSGML v1.0//DE
+BEGIN:VEVENT
+UID:${crypto.randomUUID()}
+DTSTAMP:${toIcalDate(event.start)}
+DTSTART:${toIcalDate(event.start)}
+DTEND:${toIcalDate(endOfDay(new Date(event.start)).toISOString())}
+SUMMARY:${event.title}
+LOCATION:${event.venue}
+URL:${event.url}
+DESCRIPTION:ACHTUNG, Uhrzeit beim Veranstalter prüfen: ${event.url}
+END:VEVENT
+END:VCALENDAR
+`
+console.log(ical)
+  return `data:text/calendar;base64,${Buffer.from(ical).toString('base64')}`
+}
