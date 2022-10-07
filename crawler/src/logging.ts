6c05da1d5ffc1920c8a1aca29d202924a1074f54
Björn Mosler <dev@bjoern.mosler.ch>
2022-10-07 16:49:16 +0200

    crawler: allow setting log level via CRAWLER_LOG_LEVEL.

--- crawler/src/logging.ts
+++ crawler/src/logging.ts
@@ -1,7 +1,7 @@
 import winston from 'winston'
 
 export const logger = winston.createLogger({
-  level: 'info',
+  level: process.env.CRAWLER_LOG_LEVEL ?? 'info',
   format: winston.format.json(),
   defaultMeta: { service: 'crawler' },
   transports: [
