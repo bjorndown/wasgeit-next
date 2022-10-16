3d4e31c62a06ee6f3941447a43f321d1294c54dc
Björn Mosler <dev@bjoern.mosler.ch>
2022-10-16 11:15:40 +0200

    crawler: Fix Turnhalle, scroll to get all events.

--- crawler/src/crawlers/turnhalle.ts
+++ crawler/src/crawlers/turnhalle.ts
@@ -7,14 +7,16 @@ export const crawler: Crawler = {
   name: 'Turnhalle',
   url: BASE_URL,
   crawl: async (page: Page) => {
-    const elements = await page.query('.event a')
+    const elements = await page.query('.event.box > a')
 
     return await Promise.all(
       elements.map(async (element) => {
         const [start, title, url] = await Promise.all([
           element.childText('h4'),
           element.childText('h2'),
-          element.getAttribute('href').then((path): string => `${BASE_URL}${path}`),
+          element
+            .getAttribute('href')
+            .then((path): string => `${BASE_URL}${path}`),
         ])
         return { start, title, url }
       })
@@ -23,5 +25,8 @@ export const crawler: Crawler = {
   prepareDate: (date: string) => {
     const cleaned = date.replaceAll(' ', '').slice(3, 11)
     return [cleaned, 'dd.MM.yy']
+  },
+  onLoad: () => {
+    document.querySelector('.tile.footer')?.scrollIntoView()
   },
 }
