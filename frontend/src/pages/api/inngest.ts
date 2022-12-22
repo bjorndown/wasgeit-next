75c36d7a87e910524d7358746697046d317ff9d4
Björn Mosler <dev@bjoern.mosler.ch>
2022-12-22 12:17:22 +0100

    frontend: add inngest signing key.

--- frontend/src/pages/api/inngest.ts
+++ frontend/src/pages/api/inngest.ts
@@ -1,10 +1,13 @@
 import { createScheduledFunction } from 'inngest'
 import { serve } from 'inngest/next'
 import { crawlVenues } from '@wasgeit/crawler/src'
+import { getEnvVar } from '@wasgeit/crawler/src/lib/env'
 
 const crawlerCron = createScheduledFunction(
   'wasgeit daily crawl',
   '0 3 * * *',
   crawlVenues
 )
-export default serve('wasgeit', [crawlerCron])
+export default serve('wasgeit', [crawlerCron], {
+  signingKey: getEnvVar('INNGEST_SIGNING_KEY'),
+})
