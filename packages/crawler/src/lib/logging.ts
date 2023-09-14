d0607c97a3f43797dfd353804b2016ef2c0729f3
bjorndown <bjorndown@smoler.ch>
2023-09-14 14:52:23 +0200

    crawler: add timestamp to logging format.

--- packages/crawler/src/lib/logging.ts
+++ packages/crawler/src/lib/logging.ts
@@ -4,7 +4,10 @@ export const LOG_FILE_PATH = './log.jsonl'
 
 export const logger = winston.createLogger({
   level: process.env.CRAWLER_LOG_LEVEL ?? 'info',
-  format: winston.format.json(),
   defaultMeta: { service: 'crawler' },
-  transports: [new winston.transports.Console()],
+  transports: [new winston.transports.Console({  format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json(),
+      winston.format.errors()
+    )})],
 })
