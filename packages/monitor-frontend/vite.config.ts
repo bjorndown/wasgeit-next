9b8a07a570fe9effc7e572e679c478f80234b6b7
bjorndown <bjorndown@smoler.ch>
2023-09-14 08:59:50 +0200

    monitor: add vercel config in vite.config.ts

--- packages/monitor-frontend/vite.config.ts
+++ packages/monitor-frontend/vite.config.ts
@@ -1,6 +1,9 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import vercel from "solid-start-vercel";
+
+const config = process.env.VERCEL ? { adapter: vercel() } : undefined;
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(config)],
 });
