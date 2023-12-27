98d63b067c905805d3a11b8cd1693559eb32e689
bjorndown <bjorndown@smoler.ch>
2023-12-27 14:02:05 +0100

    chore(crawler): use buildah for image build.

--- packages/crawler/build-and-push.sh
+++ packages/crawler/build-and-push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 set -o errexit
-podman build -t registry.digitalocean.com/trisolaris/wasgeit/crawler:production -f ./Dockerfile ../..
-podman push registry.digitalocean.com/trisolaris/wasgeit/crawler:production
+buildah build -t registry.digitalocean.com/trisolaris/wasgeit/crawler:production -f ./Dockerfile ../..
+buildah push registry.digitalocean.com/trisolaris/wasgeit/crawler:production
